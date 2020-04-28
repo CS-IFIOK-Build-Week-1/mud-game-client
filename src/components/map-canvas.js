6e61ca220208f3b8d4cabea1cb8086466c949980
Evans Ibok <evansibok@gmail.com>
2020-04-28 17:06:26 +0100

    complete layout and onclick for direction buttons

--- src/components/map-canvas.js
+++ src/components/map-canvas.js
@@ -21,26 +21,54 @@ const CanvasCon = styled.canvas`
   height: auto;
 `;
 
-const GameInfoCon = styled.div`
-  border: 4px solid #08E152;
-  width: 400px;
-  height: 250px;
-`;
-
-const ControlCon = styled.div`
-  border: 4px solid #08E152;
-  width: 400px;
-  height: 200px;
-  margin-top: 2em;
-`;
-
 const RightCon = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 2em;
 `;
 
+
+const GameInfoCon = styled.div`
+  border: 4px solid #08E152;
+  width: 400px;
+  height: 300px;
+`;
+
+const ControlCon = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 4px solid #08E152;
+  width: 400px;
+  height: auto;
+  margin-top: 2em;
+  overflow: hidden;
+
+  img {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`;
+
 function MapCanvas(props) {
+
+  const moveWest = evt => {
+    console.log("moved West!")
+  }
+
+  const moveNorth = evt => {
+    console.log("moved North!")
+  }
+
+  const moveEast = evt => {
+    console.log("moved East!")
+  }
+
+  const moveSouth = evt => {
+    console.log("moved South!")
+  }
+
   return (
     <DashCon>
       <CanvasCon>
@@ -54,10 +82,31 @@ function MapCanvas(props) {
         </GameInfoCon>
 
         <ControlCon>
-          <img src={north} alt="north" />
-          <img src={east} alt="east" />
-          <img src={south} alt="south" />
-          <img src={west} alt="west" />
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '1em'
+          }}>
+            <img src={north} alt="north" onClick={moveNorth} />
+          </div>
+
+          <div style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginRight: '6em',
+            marginLeft: '6em'
+          }}>
+            <img src={west} alt="west" onClick={moveWest} />
+            <img src={east} alt="east" onClick={moveEast} />
+          </div>
+
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginBottom: '1em'
+          }}>
+            <img src={south} alt="south" onClick={moveSouth} />
+          </div>
         </ControlCon>
       </RightCon>
     </DashCon>
