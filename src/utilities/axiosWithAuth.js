c943ddb93d202ba73efdba869672432809e16ee1
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-05-01 10:07:26 +0500

    redux stuff

--- src/utilities/axiosWithAuth.js
+++ src/utilities/axiosWithAuth.js
@@ -1,14 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosWithAuth = () => {
-  const key = localStorage.getItem('token');
+  const key = localStorage.getItem("token");
 
   const axiosInstance = axios.create({
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': key ? `${key}` : null
+      "Content-Type": "application/json",
+      Authorization: `Token ${key ? `${key}` : null}`,
     },
   });
 
   return axiosInstance;
 };
+export default axiosWithAuth;
